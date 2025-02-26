a0f27e02adb5548a0e66e0774d73749e82fadff7
AleCarrera11 <alejandra.carrera@correo.unimet.edu.ve>
2025-02-25 22:49:07 -0400

    Cambio de nombre Navigation1

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,12 @@
 //import React from "react";
-import { Navigation } from "./Navigation";
+import { Navigation1 } from "./Navigation1";
 import styles from "./HomePage.module.css";
 
 export default function HomePage() {
   return (
     <main className={styles.homepage}>
       <div className={styles.container}>
-        <Navigation />
+        <Navigation1 />
    
       </div>
     </main>
